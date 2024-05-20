8b54ff56ba1c94cbf9404e84d199928ca06f4e7b
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-05-20 19:18:48 +0530

    more left

---
+++ frontend/src/components/Certificate.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Certificate.css'
+
+
+const Certificate = () => {
+  return (
+    <div className='certificate-head'>
+      <h1 >Unlock <span>6 Certificates</span> & <span>Internship Opportunities!</span></h1>
+    </div>
+  )
+}
+
+export default Certificate
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,7 +14,6 @@ const Home = () => {
       <Curriculum/>
       <Refund/>
       <Testimonials/>
-      
     </div>
   )
 }
